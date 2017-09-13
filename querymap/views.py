52cdb1ee55a858611d0c34c4839116ade21bd5c2
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-09-13 12:59:00 +0100

    Stream map query result

--- querymap/views.py
+++ querymap/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, StreamingHttpResponse
 from django.conf import settings
 import pgmap
 import io
@@ -13,6 +13,34 @@ p = pgmap.PgMap(b"dbname={} user={} password='{}' hostaddr={} port={}".format(de
 	str(defaultDb["PREFIX"]), str(defaultDb["PREFIX_TEST"]))
 
 # Create your views here.
+
+class MapQueryResponse(object):
+	def __init__(self, bbox):
+		self.sio = io.BytesIO()
+		self.enc = pgmap.PyOsmXmlEncode(self.sio)
+		if p.pgMapQuery.Start(bbox, self.enc)<0:
+			raise RuntimeError("Map query failed to start")
+		self.complete = False
+
+	def __iter__(self):
+		return self
+
+	def next(self):
+		if self.complete:
+			raise StopIteration()
+	
+		ret = p.pgMapQuery.Continue()
+
+		if ret < 0:
+			raise RuntimeError("Map query error")
+		if ret == 1:
+			self.complete = True			
+
+		buff = self.sio.getvalue()
+		self.sio = io.BytesIO() #Reset buffer 
+		self.enc.SetOutput(self.sio)
+
+		return buff
 
 def index(request):
 	bbox = request.GET.get('bbox', None)
@@ -33,10 +61,6 @@ def index(request):
 		response["Error"] = err
 		return response
 
-	sio = io.BytesIO()
-	enc = pgmap.PyOsmXmlEncode(sio)
-	p.MapQuery(bbox, 50000, enc)
-
-	return HttpResponse(sio.getvalue(), content_type='text/xml')
+	return StreamingHttpResponse(MapQueryResponse(bbox), content_type='text/xml')
 
 
