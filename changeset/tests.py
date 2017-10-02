ca947b30eff44442509e911365bdcd99f66304e1
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-10-03 00:10:05 +0100

    Add more complete upload create test

--- changeset/tests.py
+++ changeset/tests.py
@@ -16,6 +16,43 @@ from querymap.views import p
 from xml.sax.saxutils import escape
 from querymap.tests import create_node
 
+def ParseOsmDiffToDict(xml):
+	out = {'node':{}, 'way':{}, 'relation':{}}
+	for diff in xml:
+		old_id, new_id, new_version = None, None, None
+		if "old_id" in diff.attrib:
+			old_id = int(diff.attrib["old_id"])
+		if "new_id" in diff.attrib:
+			new_id = int(diff.attrib["new_id"])
+		if "new_version" in diff.attrib:
+			new_version = int(diff.attrib["new_version"])
+
+		out[diff.tag][old_id] = (new_id, new_version)
+	return out
+
+def GetObj(p, objType, objId):
+	t = p.GetTransaction(b"ACCESS SHARE")
+	osmData = pgmap.OsmData() #Watch out, this goes out of scope!
+	t.GetObjectsById(objType.encode("UTF-8"), [objId], osmData)
+	del t
+	objs = None
+	if objType == "node":
+		objs = osmData.nodes
+		if len(objs) == 0:
+			return None
+		return pgmap.OsmNode(objs[0])
+	if objType == "way":
+		objs = osmData.ways
+		if len(objs) == 0:
+			return None
+		return pgmap.OsmWay(objs[0])
+	if objType == "relation":
+		objs = osmData.relations
+		if len(objs) == 0:
+			return None
+		return pgmap.OsmRelation(objs[0])
+	return None
+
 # Create your tests here.
 # alter user microcosm with createdb;
 # python manage.py test changeset --keep
@@ -263,12 +300,9 @@ class ChangesetTestCase(TestCase):
 		self.assertEqual(int(ndiff.attrib["old_id"]), -5393)
 		self.assertEqual(int(ndiff.attrib["new_version"]), 1)
 		self.assertEqual(int(ndiff.attrib["new_id"])>0, True)
-
-		t = p.GetTransaction(b"ACCESS SHARE")
-		osmData = pgmap.OsmData()
-		t.GetObjectsById(b"node", [int(ndiff.attrib["new_id"])], osmData)
-		self.assertEqual(len(osmData.nodes), 1)
-		del t
+		
+		dbNode = GetObj(p, "node", int(ndiff.attrib["new_id"]))
+		self.assertEqual(dbNode is not None, True)
 
 	def test_upload_modify_single_node(self):
 
@@ -296,13 +330,7 @@ class ChangesetTestCase(TestCase):
 		self.assertEqual(int(ndiff.attrib["new_version"]), node.metaData.version+1)
 		self.assertEqual(int(ndiff.attrib["new_id"]), node.objId)
 
-		t = p.GetTransaction(b"ACCESS SHARE")
-		osmData = pgmap.OsmData()
-		t.GetObjectsById(b"node", [node.objId], osmData)
-		self.assertEqual(len(osmData.nodes), 1)
-		del t
-
-		dbNode = osmData.nodes[0]
+		dbNode = GetObj(p, "node", node.objId)
 		self.assertEqual(abs(dbNode.lat-50.80)<1e-6, True)
 		self.assertEqual(abs(dbNode.lon+1.05)<1e-6, True)
 		self.assertEqual(len(dbNode.tags), 1)
@@ -329,11 +357,8 @@ class ChangesetTestCase(TestCase):
 		ndiff = xml[0]
 		self.assertEqual(int(ndiff.attrib["old_id"]), node.objId)
 
-		t = p.GetTransaction(b"ACCESS SHARE")
-		osmData = pgmap.OsmData()
-		t.GetObjectsById(b"node", [node.objId], osmData)
-		self.assertEqual(len(osmData.nodes), 0)
-		del t
+		dbNode = GetObj(p, "node", node.objId)
+		self.assertEqual(dbNode is None, True)
 
 	def test_upload_create_way(self):
 
@@ -343,10 +368,10 @@ class ChangesetTestCase(TestCase):
 		<create>
 		  <node changeset="{0}" id="-5393" lat="50.79046578105" lon="-1.04971367626" />
 		  <node changeset="{0}" id="-5394" lat="50.81" lon="-1.051" />
-		  <way changeset="{0}">
+		  <way changeset="{0}" id="-434">
 		   <tag k="note" v="Just a way"/>
 		   <nd ref="-5393"/>
-		   <nd ref="-5393"/>
+		   <nd ref="-5394"/>
 		  </way>
 		</create>
 		</osmChange>""".format(cs.id)
@@ -357,9 +382,87 @@ class ChangesetTestCase(TestCase):
 			print response.content
 		self.assertEqual(response.status_code, 200)
 
-		print response.content
 		xml = fromstring(response.content)
 		self.assertEqual(len(xml), 3)
+		diffDict = ParseOsmDiffToDict(xml)
+		self.assertEqual(-5393 in diffDict["node"], True)
+		self.assertEqual(-5394 in diffDict["node"], True)
+		self.assertEqual(-434 in diffDict["way"], True)
+		
+		newWayId, newWayVersion = diffDict["way"][-434]
+
+		self.assertEqual(newWayVersion, 1)
+		newWay = GetObj(p, "way", newWayId)
+		self.assertEqual(newWay is not None, True)
+		for ref in list(newWay.refs):
+			self.assertEqual(ref > 0, True)
+
+	def test_upload_create_complex(self):
+
+		cs = Changeset.objects.create(user=self.user, tags={"foo": "me"}, is_open=True)
+
+		node = create_node(self.user.id, self.user.username)
+
+		xml = """<osmChange version="0.6" generator="JOSM">
+		<create>
+		  <node id='-3912' changeset='{0}' lat='50.78673385857' lon='-1.04730886255'>
+			<tag k='abc' v='def' />
+		  </node>
+		  <node id='-3910' changeset='{0}' lat='50.7865119298' lon='-1.04843217891' />
+		  <node id='-3909' changeset='{0}' lat='50.78724872927' lon='-1.04808114255' />
+		  <way id='-3911' changeset='{0}'>
+			<nd ref='-3909' />
+			<nd ref='-3910' />
+			<nd ref='-3912' />
+			<nd ref='{1}' />
+			<tag k='ghi' v='jkl' />
+		  </way>
+		  <relation id='-3933' changeset='{0}'>
+			<member type='way' ref='-3911' role='lmn' />
+			<member type='node' ref='-3909' role='opq' />
+			<tag k='rst' v='uvw' />
+		  </relation>
+		  <relation id='-3934' changeset='{0}'>
+			<member type='way' ref='-3911' role='lmn' />
+			<member type='relation' ref='-3933' role='opq' />
+			<tag k='rst' v='xyz' />
+		  </relation>
+		</create>
+		</osmChange>""".format(cs.id, node.objId)
+
+		response = self.client.post(reverse('upload', args=(cs.id,)), xml, 
+			content_type='text/xml')
+		if response.status_code != 200:
+			print response.content
+		self.assertEqual(response.status_code, 200)
+
+		xml = fromstring(response.content)
+		diffDict = ParseOsmDiffToDict(xml)
+		
+		way = GetObj(p, "way", diffDict["way"][-3911][0])
+		wayRefs = list(way.refs)
+		for diffId, diffVer in diffDict["node"].values():
+			self.assertEqual(diffId in wayRefs, True)
+		self.assertEqual(node.objId in wayRefs, True)
+
+		wayTags = dict(way.tags)
+		self.assertEqual(wayTags, {'ghi': 'jkl'})
+
+		rel1 = GetObj(p, "relation", diffDict["relation"][-3933][0])
+		rel1Refs = zip(list(rel1.refTypeStrs), list(rel1.refIds), list(rel1.refRoles))
+		self.assertEqual(("way", diffDict["way"][-3911][0], "lmn") in rel1Refs, True)
+		self.assertEqual(("node", diffDict["node"][-3909][0], "opq") in rel1Refs, True)
+
+		rel1Tags = dict(rel1.tags)
+		self.assertEqual(rel1Tags, {'rst': 'uvw'})
+
+		rel2 = GetObj(p, "relation", diffDict["relation"][-3934][0])
+		rel2Refs = zip(list(rel2.refTypeStrs), list(rel2.refIds), list(rel2.refRoles))
+		self.assertEqual(("way", diffDict["way"][-3911][0], "lmn") in rel2Refs, True)
+		self.assertEqual(("relation", diffDict["relation"][-3933][0], "opq") in rel2Refs, True)
+
+		rel2Tags = dict(rel2.tags)
+		self.assertEqual(rel2Tags, {'rst': 'xyz'})
 
 	def tearDown(self):
 		u = User.objects.get(username = self.username)
