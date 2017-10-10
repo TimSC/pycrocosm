3c4489589753b522e97f27892c4d32b113166a3c
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-10-10 06:07:11 +0100

    Enable user registration

--- register/views.py
+++ register/views.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import forms
 
 # Create your views here.
 
+# Based on https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+
 def index(request):
-	return render(request, 'register/index.html')
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = forms.RegisterForm()
+    return render(request, 'register/index.html', {'form': form})
 
