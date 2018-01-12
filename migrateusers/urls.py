06e056b993c9b1cb4a9cc788a1e2558c78f41e2d
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2018-01-12 21:35:01 +0000

    Import legacy accounts into django #8

---
+++ migrateusers/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import print_function
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'migrateusers'
+urlpatterns = [
+    url(r'/', views.index, name='index'),
+]
+
---
+++ migrateusers/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+def index(request):
+
+	return render(request, 'migrateusers/form.html', {})
+
